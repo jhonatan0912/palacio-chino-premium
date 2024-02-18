082c64f625ec8855020756bb08314286cb9210f2
Jhonatan <jhoonatan_0912@hotmail.com>
2024-02-18 13:18:48 -0500

    Updates

--- src/app/shared/components/categories-menu/categories-menu.component.ts
+++ src/app/shared/components/categories-menu/categories-menu.component.ts
@@ -1,6 +1,8 @@
 import { NgClass } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, DestroyRef, Input, OnInit, WritableSignal, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ViewComponent } from '@core/view-component';
+import { CategoriesProxy, CategoryDto } from '@shared/proxies/categories.proxies';
 
 @Component({
   selector: 'categories-menu',
@@ -9,23 +11,32 @@ import { ViewComponent } from '@core/view-component';
   templateUrl: './categories-menu.component.html',
   styleUrls: ['./categories-menu.component.scss'],
 })
-export class CategoriesMenuComponent extends ViewComponent {
+export class CategoriesMenuComponent extends ViewComponent implements OnInit {
 
+  private categoriesProxy = inject(CategoriesProxy);
+  private destroyRef = inject(DestroyRef);
 
   selectedId!: string;
-  menuOptions = [
-    { id: '1', icon: '/assets/img-promotion.svg', name: 'Promociones' },
-    { id: '2', icon: '/assets/img-combos.svg', name: 'Combos' },
-    { id: '3', icon: '/assets/img-sopas.svg', name: 'Sopas' },
-    { id: '4', icon: '/assets/img-carta.svg', name: 'A la carta' },
-    { id: '5', icon: 'https://cdn-icons-png.flaticon.com/128/6806/6806058.png', name: 'Banquetes' },
-    { id: '6', icon: '/assets/img-bebida.svg', name: 'Bebidas' },
-    { id: '7', icon: '/assets/img-postre.svg', name: 'Postres' },
-  ];
+  menuOptions!: WritableSignal<CategoryDto[]>;
 
   constructor() {
     super();
-    this.selectedId = this.menuOptions[0].id;
+    // this.selectedId = this.menuOptions[0].id;
+  }
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories(): void {
+    this.categoriesProxy.getAll()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (categories) => {
+          this.menuOptions.set(categories);
+          console.log(this.menuOptions());
+        }
+      });
   }
 
   navigateToCategory(id: string): void {
