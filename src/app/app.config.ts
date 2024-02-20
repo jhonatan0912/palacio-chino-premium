65100cea4fe3c3a6649e96aa2f35d00f5d4a131b
Jhonatan <jhoonatan_0912@hotmail.com>
2024-02-19 19:05:32 -0500

    Add errorInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,15 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { APP_INITIALIZER, ApplicationConfig, Injector } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideIonicAngular } from '@ionic/angular/standalone';
 import { routes } from './app.routes';
 import { authInterceptor } from './auth.interceptor';
+import { errorInterceptor } from './error.interceptor';
 import { appInitializer } from './initializer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
     provideRouter(routes, withComponentInputBinding()),
     provideIonicAngular({
       mode: 'ios',
