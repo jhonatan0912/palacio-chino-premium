b97494d7fadcb8ae108180dfdc5dcf80e142e3f1
Jhonatan <jhoonatan_0912@hotmail.com>
2024-03-15 11:36:16 -0500

    global style updates

---
+++ src/app/profile/orders/order-item/order-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'order-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-item.component.html',
+  styleUrls: ['./order-item.component.scss']
+})
+export class OrderItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/profile/orders/orders.component.ts
+++ src/app/profile/orders/orders.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { OrderItemComponent } from './order-item/order-item.component';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [],
+  imports: [OrderItemComponent],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.scss'
 })
