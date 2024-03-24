627c9b3a46e0a9bb375dbce13c4280c1a43dca63
Jhonatan <jhoonatan_0912@hotmail.com>
2024-03-23 20:58:00 -0500

    update profile routes

---
+++ src/app/profile/profile.routes.ts
@@ -0,0 +1,32 @@
+import { Route, Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./profile.component').then(p => p.ProfileComponent),
+    children: [
+      {
+        path: 'addresses',
+        children: [
+          {
+            path: '',
+            loadComponent: () => import('./addresses/addresses.component').then(p => p.AddressesComponent),
+          },
+          {
+            path: 'add',
+            loadComponent: () => import('./addresses/address-form/address-form.component').then(p => p.AddressFormComponent)
+          }
+        ]
+      },
+      {
+        path: 'orders',
+        loadComponent: () => import('./orders/orders.component').then(p => p.OrdersComponent)
+      },
+      {
+        path: 'personal-information',
+        loadComponent: () => import('./personal-information/personal-information.component').then(p => p.PersonalInformationComponent)
+      },
+      { path: '**', redirectTo: 'personal-information', pathMatch: 'full' }
+    ]
+  }
+];
