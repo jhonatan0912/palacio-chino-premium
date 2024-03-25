0be1a0e8ef9513eb92b09d6120822ba612dc32c2
Jhonatan <jhoonatan_0912@hotmail.com>
2024-03-24 21:52:24 -0500

    updates

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { IonRouterOutlet } from "@ionic/angular/standalone";
+import { AsideComponent } from './components/aside/aside.component';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [RouterOutlet,],
+  imports: [RouterOutlet, AsideComponent],
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
--- src/app/checkout/choose-address/choose-address.component.ts
+++ src/app/checkout/choose-address/choose-address.component.ts
@@ -1,17 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
+import { IonRadioGroup, IonRadio, IonSpinner } from '@ionic/angular/standalone';
+import { AddressesService } from '@profile/services/addresses.service';
+import { AddressDto, AddressesProxy } from '@shared/proxies/addresses.proxies';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-choose-address',
   standalone: true,
-  imports: [],
+  imports: [IonSpinner, IonRadio, FormsModule, IonRadioGroup],
   templateUrl: './choose-address.component.html',
-  styleUrls: ['./choose-address.component.css']
+  styleUrls: ['./choose-address.component.scss']
 })
 export class ChooseAddressComponent implements OnInit {
 
-  constructor() { }
+  private readonly _addressesProxy = inject(AddressesProxy);
+  private readonly _addressesService = inject(AddressesService);
+  private readonly _destroyRef = inject(DestroyRef);
+
+  busy: boolean = false;
+  addresses = signal<AddressDto[]>([]);
+  address: AddressDto = new AddressDto({
+    id: '',
+    district: '',
+    type: '',
+    street: '',
+    number: '',
+    phone: '',
+    reference: '',
+  });
 
   ngOnInit() {
+    this.onGetAddresses();
   }
 
+  onGetAddresses(): void {
+    if (this._addressesService.loaded()) {
+      this.addresses.set(this._addressesService.addresses());
+    } else {
+      this.busy = true;
+      this._addressesProxy.getAll()
+        .pipe(
+          takeUntilDestroyed(this._destroyRef),
+          finalize(() => this.busy = false)
+        )
+        .subscribe({
+          next: (addresses) => {
+            this.addresses.set(addresses);
+            this._addressesService.loaded.set(true);
+            this._addressesService.addresses.set(addresses);
+          }
+        });
+    }
+  }
+
+  onSelect(address: AddressDto): void {
+    this.address = address;
+  }
 }
