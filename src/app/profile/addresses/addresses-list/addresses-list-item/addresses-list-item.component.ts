36d8f797ba4c0290db8bc968ef409affadf6f674
Jhonatan <jhoonatan_0912@hotmail.com>
2024-03-25 22:58:32 -0500

    Update Addresses

--- src/app/profile/addresses/addresses-list/addresses-list-item/addresses-list-item.component.ts
+++ src/app/profile/addresses/addresses-list/addresses-list-item/addresses-list-item.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject, input } from '@angular/core';
+import { ViewComponent } from '@core/view-component';
+import { IonIcon } from "@ionic/angular/standalone";
+import { AddressesService } from '@profile/services/addresses.service';
+import { AddressDto, AddressesProxy } from '@shared/proxies/addresses.proxies';
 
 @Component({
   selector: 'addresses-list-item',
   standalone: true,
-  imports: [],
+  imports: [IonIcon,],
   templateUrl: './addresses-list-item.component.html',
   styleUrls: ['./addresses-list-item.component.scss']
 })
-export class AddressesListItemComponent implements OnInit {
+export class AddressesListItemComponent extends ViewComponent {
 
-  // constructor() { }
+  private readonly _addressesProxy = inject(AddressesProxy);
+  private readonly _addressesService = inject(AddressesService);
 
-  ngOnInit() {
+  address = input.required<AddressDto>();
+
+  constructor() {
+    super();
+  }
+
+  onEdit(id: string): void {
+    this.navigation.forward('/profile/addresses/form/edit', { id });
+  }
+
+  onDelete(): void {
+    this._addressesProxy.delete(this.address().id!)
+      .subscribe({
+        next: (response) => {
+          this._addressesService.addresses.update((prev) => prev.filter(address => address.id !== response.id));
+        }
+      });
   }
 
 }
--- src/app/profile/addresses/addresses-list/addresses-list.component.ts
+++ src/app/profile/addresses/addresses-list/addresses-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { ViewComponent } from '@core/view-component';
 import { ButtonComponent } from '@lib/button/button.component';
 import { AddressesListItemComponent } from "./addresses-list-item/addresses-list-item.component";
+import { AddressDto, AddressesProxy } from '@shared/proxies/addresses.proxies';
+import { AddressesService } from '@profile/services/addresses.service';
 
 @Component({
   selector: 'addresses-list',
@@ -10,9 +12,33 @@ import { AddressesListItemComponent } from "./addresses-list-item/addresses-list
   templateUrl: './addresses-list.component.html',
   styleUrls: ['./addresses-list.component.scss']
 })
-export class AddressesListComponent extends ViewComponent {
+export class AddressesListComponent extends ViewComponent implements OnInit {
 
-  addAddresses() {
+  private readonly _addressesProxy = inject(AddressesProxy);
+  private readonly _addressesService = inject(AddressesService);
+
+  addresses = signal<AddressDto[]>([]);
+
+  ngOnInit(): void {
+    this.onGetAddresses();
+  }
+
+  onGetAddresses(): void {
+    if (this._addressesService.loaded()) {
+      this.addresses.set(this._addressesService.addresses());
+    } else {
+      this._addressesProxy.getAll()
+        .subscribe({
+          next: (addresses) => {
+            this.addresses.set(addresses);
+            this._addressesService.addresses.set(addresses);
+            this._addressesService.loaded.set(true);
+          }
+        });
+    }
+  }
+
+  onAddAddress(): void {
     this.navigation.forward('/profile/addresses/add');
   }
 
