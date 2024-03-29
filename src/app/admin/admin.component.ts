c8324b674ad39b9f03b510c3fd3156c623ab1410
Jhonatan <jhoonatan_0912@hotmail.com>
2024-03-28 23:00:35 -0500

    feat: Remove Admin header component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,14 +1,13 @@
 import { NgClass } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AdminHeaderComponent } from "./components/header/header.component";
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AdminWebsocketsService } from './services/websockets.service';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, AdminHeaderComponent, NgClass],
+  imports: [RouterOutlet, SidebarComponent, NgClass],
   providers: [AdminWebsocketsService],
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
