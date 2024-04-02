d26a080cbe06235b3e4b207954c9eaf1960790b7
Jhonatan <jhoonatan_0912@hotmail.com>
2024-04-01 19:46:25 -0500

    feat: Update

--- src/app/profile/addresses/addresses-list/addresses-list-item/addresses-list-item.component.ts
+++ src/app/profile/addresses/addresses-list/addresses-list-item/addresses-list-item.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, inject, input } from '@angular/core';
 import { ViewComponent } from '@core/view-component';
-import { IonIcon } from "@ionic/angular/standalone";
+import { IonIcon, IonSpinner } from "@ionic/angular/standalone";
 import { AddressesService } from '@profile/services/addresses.service';
 import { AddressDto, AddressesProxy } from '@shared/proxies/addresses.proxies';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'addresses-list-item',
   standalone: true,
-  imports: [IonIcon,],
+  imports: [IonSpinner, IonIcon,],
   templateUrl: './addresses-list-item.component.html',
   styleUrls: ['./addresses-list-item.component.scss']
 })
@@ -16,6 +17,7 @@ export class AddressesListItemComponent extends ViewComponent {
   private readonly _addressesProxy = inject(AddressesProxy);
   private readonly _addressesService = inject(AddressesService);
 
+  busy: boolean = false;
   address = input.required<AddressDto>();
 
   constructor() {
@@ -27,10 +29,15 @@ export class AddressesListItemComponent extends ViewComponent {
   }
 
   onDelete(): void {
+    this.busy = true;
     this._addressesProxy.delete(this.address().id!)
+      .pipe(finalize(() => this.busy = false))
       .subscribe({
         next: (response) => {
           this._addressesService.addresses.update((prev) => prev.filter(address => address.id !== response.id));
+        },
+        error: (err) => {
+          this.notify.error(err.message, 1500);
         }
       });
   }
